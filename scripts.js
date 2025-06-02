d48c363916476b5e8d3664f5ddb4e87ee3daab36
sanatan sethi <sanatan_dev@outlook.com>
2025-06-02 21:23:42 +0530

    handled adding books to website using js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const htmlBookShelf = document.querySelector(".js-book-shelf");
+
 const myLibrary = [];
 function Book(name, author, pages, status){
     this.uid = crypto.randomUUID();
@@ -16,5 +18,25 @@ myLibrary.push(book2);
 const book3 = new Book("book3", "bot3", 205, "not read");
 myLibrary.push(book3);
 
+const book4 = new Book("book4", "someone else", 554, "read");
+myLibrary.push(book4);
 
-console.log(myLibrary)
+const veryLongBook = new Book("veryLongBook", "myself", 69, "not read");
+myLibrary.push(veryLongBook);
+
+function showBooks(arr){
+    arr.forEach((book, i) => {
+        htmlBookShelf.innerHTML +=`
+        <div class="book">
+            <img class="book-image" src="images/book.png" alt="not found">
+            <div class="book-label">
+                <h>( ${i+1} )</h1>
+                <div><b>Name:</b> ${book.name}</div>
+                <div><b>author:</b> ${book.author}</div>
+                <div><b>pages:</b> ${book.pages} </div>
+            </div>
+        </div>`
+    });
+}
+
+showBooks(myLibrary);
