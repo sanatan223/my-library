4e5e635db9ee8ea56336a6b91b391d1b551445c0
sanatan sethi <sanatan_dev@outlook.com>
2025-06-05 08:06:16 +0530

    gave functionality to cancel and confirm button in the input form

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 const htmlBookShelf = document.querySelector(".js-book-shelf");
-const htmlForm = document.querySelector(".js-form")
-const htmlAddBtn = document.querySelector(".js-add-button")
+const htmlForm = document.querySelector(".js-form");
+const htmlAddBtn = document.querySelector(".js-add-button");
+const htmlCancelBtn = document.querySelector(".js-cancel-btn");
+const htmlConfirmBtn = document.querySelector(".js-confirm-btn");
+const htmlNameInput = document.querySelector(".js-name-input");
+const htmlAuthorInput = document.querySelector(".js-author-input");
+const htmlPagesInput = document.querySelector(".js-pages-input");
 
 const myLibrary = [];
 function Book(name, author, pages, status){
@@ -11,27 +16,34 @@ function Book(name, author, pages, status){
     this.status = status;
 }
 
-const book1 = new Book("book1", "bot1", 503, "not read");
-myLibrary.push(book1);
-
-const book2 = new Book("book2", "bot2", 703, "read");
-myLibrary.push(book2);
-
-const book3 = new Book("book3", "bot3", 205, "not read");
-myLibrary.push(book3);
-
-const book4 = new Book("book4", "someone else", 554, "read");
-myLibrary.push(book4);
-
-const veryLongBook = new Book("veryLongBook", "myself", 69, "not read");
-myLibrary.push(veryLongBook);
-
 htmlAddBtn.addEventListener("click", () => {
     htmlForm.showModal()
     console.log("works")
 })
 
+htmlCancelBtn.addEventListener("click", () => {
+    htmlForm.close()
+    console.log("closed it !")
+})
+
+htmlConfirmBtn.addEventListener("click", () => {
+    let name = htmlNameInput.value;
+    let author = htmlAuthorInput.value;
+    let pages = htmlPagesInput.value;
+    let status = "not read";
+    addBookToLibrary(name, author, pages, status);
+    htmlForm.close();
+
+})
+
+function addBookToLibrary(name, author, pages, status){
+    let tempBook = new Book(name, author, pages, status);
+    myLibrary.push(tempBook);
+    showBooks(myLibrary);
+}
+
 function showBooks(arr){
+    htmlBookShelf.innerHTML = "";
     arr.forEach((book, i) => {
         htmlBookShelf.innerHTML +=`
         <div class="book">
